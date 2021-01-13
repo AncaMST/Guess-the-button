720601417b71df90d028224c7902d2f9329349f0
Anca Musat <ank17_2003@yahoo.com>
2021-01-13 19:54:34 +0200

    Solved the uniqueness of the winning buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,32 @@
-function generateButtons() {
-    var numberButtons=document.getElementById('numberOfButtons').value;
-    document.getElementById("sel").innerHTML = "Select a button!";
-    var index, element, text;
-    for (index=1; index<=numberButtons; ++index) {
-        element = document.createElement("button");
-        element.setAttribute("id", index);
-        element.setAttribute("onclick", "replyClick(this.id)");
-        text=document.createTextNode(index);
-        element.appendChild(text);
-        document.body.appendChild(element);
-    }       
-}
+    var array = [];
 
-function replyClick(clickedId) {
-    var name;
-    var max=document.getElementById('numberOfButtons').value;
-    var selected=Math.floor(Math.random()*max)+1;
-    name = (clickedId==selected) ? ("winner") : ("not the winner");
-    alert(name);
-}
+    function generateButtons() {
+        var numberButtons=document.getElementById('numberOfButtons').value;
+        document.getElementById("sel").innerHTML = "Select a button!";
+        var index, element, text;
+        for (index=1; index<=numberButtons; ++index) {
+            element = document.createElement("button");
+            element.setAttribute("id", index);
+            element.setAttribute("onclick", "replyClick(this.id)");
+            text=document.createTextNode(index);
+            element.appendChild(text);
+            document.body.appendChild(element);
+        }
+    }
+
+    function replyClick(clickedId) {
+        var name;
+        var max=document.getElementById('numberOfButtons').value;
+        var selected=Math.floor(Math.random()*max)+1;
+        if (array.indexOf(selected)===-1) {
+            array.push(selected);
+        } else if (array.length<=max) {
+            while (array.indexOf(selected)!==-1) {
+                selected=Math.floor(Math.random()*max)+1;
+            }
+            array.push(selected);
+        }
+        name = (clickedId==selected) ? ("the winner") : ("not the winner");
+        alert(selected+" "+name);
+    }
+
